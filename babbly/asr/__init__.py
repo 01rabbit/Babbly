dce2f7c418c52bcd6ba7d3bbb46eb0ed162aadd0
Mr.Rabbit <appleseedj073@gmail.com>
2026-08-13 10:28:46 +0900

    fix: lazy-load ASR backend dependencies

--- babbly/asr/__init__.py
+++ babbly/asr/__init__.py
@@ -1,4 +1,11 @@
-from .factory import create_asr
 from .types import ASRResult
+
+
+def create_asr(config):
+    """Create the configured ASR backend without importing ML/audio stacks at package import time."""
+    from .factory import create_asr as _create_asr
+
+    return _create_asr(config)
+
 
 __all__ = ["ASRResult", "create_asr"]
